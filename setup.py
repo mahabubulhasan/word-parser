7c960f1bee8512b90d186340b980bb644e869086
Mahabubul Hasan <codehasan@gmail.com>
2019-04-12 15:49:32 +0600

    setup instructions

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='word_parser',
+    version='0.1.0',
+    packages=[''],
+    url='http://uzzal.com',
+    license='MIT',
+    author='Mahabubul Hasan',
+    author_email='uzzal@outlook.com',
+    description='Vocabulary parser'
+)
